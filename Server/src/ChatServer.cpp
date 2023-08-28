ce8a07b2e191413e3c59260a1736680efa8dfc15
Jaroslaw <roszyk.jarek@gmail.com>
2023-08-28 22:13:33 +0200

    add: Add boost with example

--- Server/src/ChatServer.cpp
+++ Server/src/ChatServer.cpp
@@ -1,8 +1,45 @@
+#include <boost/asio.hpp>
+#include <boost/beast.hpp>
 #include <iostream>
-#include "Server.hpp"
 
+namespace net = boost::asio;
+namespace http = boost::beast::http;
+using tcp = boost::asio::ip::tcp;
+
+auto handle_request(tcp::socket& socket) -> void
+{
+    http::response<http::string_body> response;
+    response.version(11);
+    response.result(http::status::ok);
+    response.set(http::field::server, "Server");
+    response.set(http::field::content_type, "text/html");
+    response.body() = "Hello, I'm an Asio and Beast server";
+    response.prepare_payload();
+
+    http::write(socket, response);
+}
+
+/*
+    How it works?
+    open web browser and type:
+    localhost:8080 or http://127.0.0.1:8080/
+*/
 auto main() -> int
 {
-    Server server;
-    server.print();
+    net::io_context ioc;
+    constexpr int PORT = 8080;
+    tcp::acceptor acceptor(ioc, tcp::endpoint(tcp::v4(), PORT));
+
+    while (true)
+    {
+        tcp::socket socket(ioc);
+        acceptor.accept(socket);
+
+        handle_request(socket);
+
+        boost::system::error_code ec;
+        socket.shutdown(tcp::socket::shutdown_send, ec);
+    }
+
+    return 0;
 }
